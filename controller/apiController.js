2e84858c2981aeb88bfeac5f4da6a30bdc82c779
ShynRii <tlong4856@gmail.com>
2021-12-20 15:38:51 +0700

    test

--- controller/apiController.js
+++ controller/apiController.js
@@ -324,14 +324,14 @@ module.exports.cancelSchedule = async (req, res) => {
         $set:
             {status: 'Cancelled', note, idStaffConfirm: user._id, timeConfirm: new Date()}
     }, {new: true}).then((schedule) => {
-        // for (let staff of staffs) {
-        //     if (staff.tokenDevice != null && staff.tokenDevice.length > 0) {
-        //         notify('Xin lỗi', `Khách hàng đã hủy lịch vì lý do ${note}`, req.user.tokenDevice)
-        //     }
-        // }
+        for (let staff of user) {
+            if (staff.tokenDevice != null && staff.tokenDevice.length > 0) {
+                notify('Xin lỗi', `Cửa hàng đã hủy lịch vì lý do ${note}`, req.user.tokenDevice)
+            }
+        }
         for (let staff of staffs) {
             if (staff.tokenDevice != null && staff.tokenDevice.length > 0) {
-                notify('Xin lỗi', `Lịch của bạn đã bị huỷ vì lý do ${note}`, staff.tokenDevice)
+                notify('Khách hàng huỷ lịch', `Khách hàng đã huỷ lịch vì lý do ${note}`, staff.tokenDevice)
             }
         }
         addNotify(`Lịch của bạn đã bị hủy`, schedule.idUser, schedule._id)
