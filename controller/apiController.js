ceb5ec4ca78c30a96db9eb50fbfed6232b3993b9
ShynRii <tlong4856@gmail.com>
2021-12-20 16:46:37 +0700

    fix title

--- controller/apiController.js
+++ controller/apiController.js
@@ -279,7 +279,7 @@ module.exports.schedule = async (req, res) => {
         return res.json({success: false, message: 'Khung giờ này đã đầy'})
     }
     if(slSchedule.length === 3){
-        return res.json({success: false, message: 'Khung giờ này đã đầy'})
+        return res.json({success: false, message: 'Khung giờ này đã đầy 2'})
     }
     let hour = timeBook.split('@')[0].split(':')[0].trim()
     let min = timeBook.split('@')[0].split(':')[1].trim()
