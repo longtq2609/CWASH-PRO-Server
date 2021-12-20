da8f64666ad5b04a3490a218e0a560ab70575d40
ShynRii <tlong4856@gmail.com>
2021-12-20 16:38:04 +0700

    check slot

--- controller/apiController.js
+++ controller/apiController.js
@@ -277,7 +277,9 @@ module.exports.schedule = async (req, res) => {
     let slSchedule = await Schedule.find({timeBook, status: "Pending"})
     if (slSchedule.length >= staffs.length) {
         return res.json({success: false, message: 'Khung giờ này đã đầy'})
-
+    }
+    if(slSchedule.length >= 3){
+        return res.json({success: false, message: 'Khung giờ này đã đầy'})
     }
     let hour = timeBook.split('@')[0].split(':')[0].trim()
     let min = timeBook.split('@')[0].split(':')[1].trim()
