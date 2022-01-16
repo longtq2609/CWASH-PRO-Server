14cea1cb6a08dc329207aad5a7b53677c11a76f9
dungndph09884 <dungndph09884@fpt.edu.vn>
2022-01-16 21:42:01 +0700

    getIntro

--- controller/introController.js
+++ controller/introController.js
@@ -3,6 +3,25 @@ let fs = require('fs');
 
 let New = require('../model/New');
 
-//Intro controller
-
+module.exports.getAllIntro = async (req, res) => {
+    await New.find({}).then((data) => {
+        res.render('introduce/Introduces', {layout: 'temp/index', title: "Bảng tin", err: false, data:data.reverse()})
+    }, (err) => {
+        res.render('error/404', {layout: 'temp/index', title: "Có lỗi xảy ra !", err: true, message: err})
+    }).catch((err) => {
+        res.render('error/404', {layout: 'temp/index', title: "Có lỗi xảy ra !", err: true, message: err})
+    })
 }
+
+module.exports.getIntroUsing = async (req, res) => {
+    await New.find({status:true}).then((data) => {
+        res.render('introduce/Introduces', {layout: 'temp/index', title: "Những tin đang sử dụng", err: false, data:data.reverse()})
+    }, (err) => {
+        res.render('error/404', {layout: 'temp/index', title: "Có lỗi xảy ra !", err: true, message: err})
+    }).catch((err) => {
+        res.render('error/404', {layout: 'temp/index', title: "Có lỗi xảy ra !", err: true, message: err})
+    })
+}
+
+
+
